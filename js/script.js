41f11e1ccc3f34acb60c5f8ac686631f09d664c1
mmurilozz <murilocsouza1@gmail.com>
2026-03-26 21:35:54 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 // DOM JS
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     // objeto no html (ícone sol ou lua)
     const toggleTheme = document.getElementById('toggleTheme');
     const rootHtml = document.documentElement;
+    // Var para menu Hamburger
+    const menuHamburger = document.getElementById('menuHamburger');
+    // Estrutura do menu
+    const menuMobile = document.getElementById('menuMobile');
+    // Verificação ou condição
+    const icon = menuHamburger?.querySelector('i');
+
+
+
 
     // Função mudar a cor do tema
     function changeTheme() {
@@ -20,5 +29,21 @@ document.addEventListener('DOMContentLoaded', () => {
     if (toggleTheme) {
         toggleTheme.addEventListener('click', changeTheme);
     }
+
+    /* Alternar menu mobile e ícone */
+    function toggleMenu() {
+        /* lista NAV */
+        const isOpen = menuMobile.classList.toggle('active');
+        //condição
+        if (icon) {
+            icon.classList.toggle('bi-list', !isOpen);
+            icon.classList.toggle('bi-x-lg', isOpen);
+        }
+    }
+    
+    if (menuHamburger) {
+        menuHamburger.addEventListener('click', toggleMenu);
+    }
+
 
 });
